eccc912ff866546f7e25905a1e43b55dcc0f58ad
Philipp Gille <philipp.gille@gmail.com>
2018-08-25 23:38:47 +0200

    Split code in the ln package in separate files
    - One for the lnd implementation, one for other code

--- ln/lnd.go
+++ ln/lnd.go
@@ -106,16 +106,3 @@ func (c LNDclient) CheckInvoice(preimage string) (bool, error) {
 	}
 	return true, nil
 }
-
-// HashPreimage hashes the Base64 preimage and encodes the hash in Base64.
-// It's the same format that's being shown by lncli listinvoices (preimage as well as hash).
-func HashPreimage(preimage string) (string, error) {
-	decodedPreimage, err := base64.StdEncoding.DecodeString(preimage)
-	if err != nil {
-		return "", err
-	}
-	hash := sha256.Sum256([]byte(decodedPreimage))
-	hashSlice := hash[:]
-	encodedHash := base64.StdEncoding.EncodeToString(hashSlice)
-	return encodedHash, nil
-}
