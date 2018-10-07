2107d04f74531408e64eb768c7333046edeff2cd
Philipp Gille <philipp.gille@gmail.com>
2018-10-07 23:36:13 +0200

    WIP

--- storage/storage.go
+++ storage/storage.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 )
 
@@ -10,4 +12,20 @@ func toJSON(v interface{}) ([]byte, error) {
 
 func fromJSON(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
+}
+
+func toBinary(v interface{}) ([]byte, error) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(v)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+func fromBinary(data []byte, v interface{}) error {
+	reader := bytes.NewReader(data)
+	decoder := gob.NewDecoder(reader)
+	return decoder.Decode(v)
 }
